697b2db30d77f5ff5c1f43a167196eba1f68806b
Kenenth Gandonou <kenenthgandonou@ip-192-168-69-231.ec2.internal>
2023-02-14 16:04:29 -0800

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -20,11 +20,9 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
-          <div>
-    
-          </div>
+          <div></div>
         </div>
       </main>
     </>
-  )
+  );
 }
